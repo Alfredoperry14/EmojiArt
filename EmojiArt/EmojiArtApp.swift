7901ae6a7dce9d044f775bcfd39e3b92ee10b355
alfredoperry14 <alfredoperry14@gmail.com>
2024-10-05 12:07:02 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Alfredo Perry on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
